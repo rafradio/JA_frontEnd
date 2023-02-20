91e81a87519c8d3ce8ea952b000c847a932ac75a
Rafail Abdyushev <rafaelab@mail.ru>
2023-02-20 17:11:37 +0300

    observer for text

--- JobAssistantFront/js/animation.js
+++ JobAssistantFront/js/animation.js
@@ -1,5 +1,8 @@
 function requestEmailForm() {
+    this.topTextBlocksLeft = document.querySelectorAll('.left');
+    this.topTextBlocksRight = document.querySelectorAll('.right');
     this.reqButton = document.querySelector('.button_landing');
+
     this.regForm = document.querySelector('.request_modal_wrap');
     this.closeBtn = document.querySelector('.close_btn');
     this.inputText = document.querySelectorAll('.form-field__input');
@@ -9,7 +12,10 @@ function requestEmailForm() {
 }
 
 requestEmailForm.prototype.initSettings = function() {
-    
+
+    // console.log(this.topTextBlocks.length);
+    // this.topTextBlocks[0].classList.add("top_text_animation");
+
     this.reqButton.addEventListener('click', () => {
         console.log("Hello world");
         this.regForm.classList.add("request_modal_wrap_on");
@@ -20,8 +26,9 @@ requestEmailForm.prototype.initSettings = function() {
     });
 
     this.closeBtn.addEventListener('click', () => {this.regForm.classList.remove("request_modal_wrap_on");});
-
     this.submitButton.addEventListener('click', () => {this.saveData()});
+
+    window.onload = () => {this.textAnimation();}
 }
 
 requestEmailForm.prototype.checking = function(x) {
@@ -39,5 +46,29 @@ requestEmailForm.prototype.saveData = function() {
     URL.revokeObjectURL(tempLink.href);
     this.regForm.classList.remove("request_modal_wrap_on");
 }
+
+requestEmailForm.prototype.textAnimation = function() {
+    
+    this.topTextBlocksLeft.forEach((item, pos) => {
+        this.animateObserver(item, "animation_left");
+    });
+    this.topTextBlocksRight.forEach((item, pos) => {
+        this.animateObserver(item, "animation_right");
+    });
+}
+
+requestEmailForm.prototype.animateObserver = function(item, className) {
+    let callback1 = (entries, observer) => {
+        entries.forEach(
+            (entry) => {
+                if (entry.isIntersecting) {
+                    item.classList.add(className);
+                }
+            }
+        )};
+    const observer = new IntersectionObserver(callback1);
+    observer.observe(item);
+}
+
 
 const myReqForm = new requestEmailForm();
